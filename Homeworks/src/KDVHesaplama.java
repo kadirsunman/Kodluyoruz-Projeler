d3b2f6dd0cc935c7b243f4f54f7c1fc777d5cd56
Kadir Sunman <kadirsunman@gmail.com>
2022-01-15 20:27:33 +0300

    Java Homework

---
+++ Homeworks/src/KDVHesaplama.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class KDVHesaplama {
+    public static void main(String[] args) {
+        //Kullanıcıdan alınan fiyatı ve hesaplama yapmak için birer ondalıklı değişken tanımlıyoruz
+        float girilenFiyat;
+        float hesaplananFiyat;
+
+        Scanner scan = new Scanner(System.in);
+
+        //Kullanıcıdan fiyat alıyoruz
+        System.out.print("Fiyat Giriniz : ");
+        girilenFiyat = scan.nextInt();
+
+        //Hesaplama bölümü
+        if(girilenFiyat > 0 && girilenFiyat < 1000)
+        {
+            hesaplananFiyat = girilenFiyat + (girilenFiyat * 0.18f);
+        }
+        else
+        {
+            hesaplananFiyat = girilenFiyat + (girilenFiyat * 0.08f);
+        }
+        System.out.println("Kdv'li hesaplanan tutar : " + hesaplananFiyat + "₺");
+
+    }
+}
